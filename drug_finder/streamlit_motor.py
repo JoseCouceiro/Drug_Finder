49e9931b34f4bd412e7440040cc8ef1406e365a4
JoseCouceiro <jose.r.couceiro@gmail.com>
2025-03-05 20:25:20 +0100

    ready to streamline and clean

--- drug_finder/streamlit_motor.py
+++ drug_finder/streamlit_motor.py
@@ -1,12 +1,10 @@
 import streamlit as st
-import random
 from drug_finder.streamlit_cima import Search
 from drug_finder.handle_data import HandleData
 from drug_finder.streamlit_print_data import PrintMedInfo
 from drug_finder.translator import Translate
 from drug_finder.streamlit_pubchem import Pubchem
 from drug_finder.db.database_handle import DataBase
-
 
 class Motor:
 
@@ -16,25 +14,80 @@ class Motor:
         self.__printer = PrintMedInfo()
         self.__translator = Translate()
         self.__pubchem = Pubchem()
-        self.__db = DataBase()
-        if 'dynamic_buttons' not in st.session_state:
-            st.session_state.dynamic_buttons = {}
-            st.session_state.dynamic_buttons_deployed = True
-        self.__nsearches = 0
+        #self.__db = DataBase()
+        if 'med_buttons' not in st.session_state:
+            st.session_state.med_buttons = {}
+            st.session_state.med_buttons_deployed = True
+        if 'comp_buttons' not in st.session_state:
+            st.session_state.comp_buttons = {}
+            st.session_state.comp_buttons_deployed = True
 
-    #FUNCIONES DE NAVEGACIÓN
-    #MENU 1
-    
+    def welcome(self):
+        st.title('\nWELCOME TO DRUG FINDER')
+        query = self.__display1()
+        if query:
+            self.__process_choice(query)
 
-    def create_dynamic_button(self, button_key, label):
-        if button_key not in st.session_state.dynamic_buttons:
-            st.session_state.dynamic_buttons[button_key] = False  # Initialize button state
+    def __display1(self):
+        st.write("\nYou may search for a medicine or an active compound:\n")
+        medicine = st.text_input('Search a medicine')
+        compound = st.text_input('Search an active compound')
+        indication = st.text_input('Search by therapeutic indication')
+        return medicine, compound, indication
 
+    def __process_choice(self, selection):
+        medicine = selection[0]
+        compound = selection[1] 
+        indication = selection[2]
+        if medicine:
+            st.write(f'**:blue[MEDICINE: ] {medicine}**')
+            __search_term = self.__searcher.find_medicines(medicine)
+            __func = self.__searcher.find_medicines
+            __posting = False
+            self.__display_medicines(__search_term, __posting)
+        if compound:
+            st.write(f'**:blue[COMPOUND: ] {compound}**')
+            __search_term = self.__searcher.find_principles(compound)
+            __func = self.__searcher.find_principles
+            __posting = False
+            self.__display_medicines(__search_term, __posting)
+        if indication:
+            st.write(f'**:blue[INDICATION: ] {indication}**')
+            __search_term = self.__searcher.find_therapeutic_indication(indication)
+            __func = self.__searcher.find_therapeutic_indication
+            __posting = True
+            self.__display_medicines(__search_term, __posting)
+        download_csv = st.button('Download as csv')
+        if download_csv: 
+            self.__handler.to_csv(self.__searcher.nombre,self.__searcher.response_dict)
+
+    def __create_med_button(self, button_key, label):
+        if button_key not in st.session_state.med_buttons:
+            st.session_state.med_buttons[button_key] = False  # Initialize button state
         # Create the button
-        if st.button(label, key=button_key):
-            st.session_state.dynamic_buttons[button_key] = True  # Update button state on click
+        try:
+            if st.button(label, key=button_key):
+                st.session_state.med_buttons[button_key] = True  # Update button state on click
+        except:
+            pass
 
-    def med_list(self, response):
+    def __create_comp_button(self, button_key, label):
+        if button_key not in st.session_state.comp_buttons:
+            st.session_state.comp_buttons[button_key] = False  # Initialize button state
+        # Create the button
+        try:
+            if st.button(label, key=button_key):
+                st.session_state.comp_buttons[button_key] = True  # Update button state on click
+        except:
+            pass
+
+    def __display_medicines(self, query, post):
+        self.__check_length(query)
+        self.__searcher.search_motor(post)    
+        #self.__handler.save_json(self.__searcher.data_json,self.__searcher.response_dict)
+        return self.__print_med_list(self.__searcher.response_dict, query)
+
+    def __print_med_list(self, response, query):
         if response == {}:
             st.write('\nNo results to show, please repeat your search')
             st.write('Check your spelling and remember that the database is in Spanish\n')
@@ -47,75 +100,22 @@ class Motor:
                 self.button_dic = dict()
                 for __med in __dic['resultados']:
                     __number_medicines +=1
-                    __key +=1
-                    if st.session_state.get('dynamic_buttons_deployed', False):
-                        self.create_dynamic_button(__key, f'{__number_medicines} ' + __med['nombre'])
-                        if st.session_state.dynamic_buttons.get(__number_medicines, False):
+                    __key = str(__med['nregistro'])
+                    if st.session_state.get('med_buttons_deployed', False):
+                        self.__create_med_button(button_key=__key, label=f'{__number_medicines} ' + __med['nombre'])
+                        if st.session_state.med_buttons.get(__med['nregistro'], False):
                             self.__searcher.find_medicines(__med['nombre'])
                             self.__searcher.search_motor(False) 
-                            self.__select_medicine(1)
-                    st.session_state.dynamic_buttons_deployed = True
-                    
+                            self.__display_compounds_info(__med['nregistro'])
+                    st.session_state.med_buttons_deployed = True      
             return __number_medicines
         
-    def __select_medicine(self, choice):
-        self.nregistro = self.__handler.find_nregistro(choice,self.__searcher.response_dict)
-
-        self.__searcher.find_medicine(self.nregistro)
-        self.__searcher.search_motor_sing()
-         
-        self.__handler.save_json_sing(self.__searcher.data_json_sing,self.__searcher.response_sing.json())
-        self.basic_info(self.__searcher.response_sing.json(), self.nregistro)
-        
-
-    def welcome(self):
-        st.title('\nWELCOME TO DRUG FINDER')
-        query = self.__display1()
-        if query:
-            self.__process_choice1(query)
-            
-        
-    def __display1(self):
-        st.write("\nYou may search for a medicine or an active compound:\n")
-        medicine = st.text_input('Search a medicine')
-        compound = st.text_input('Search an active compound')
-        indication = st.text_input('Search by therapeutic indication')
-        return medicine, compound, indication
-    
-    def __display_medicines(self, query, func, post):
-        self.__check_length(query, func)
-        self.__searcher.search_motor(post)    
-        self.__handler.save_json(self.__searcher.data_json,self.__searcher.response_dict)
-        med_list = self.med_list(self.__searcher.response_dict)
-        return med_list
-    
-    def __process_choice1(self, selection):
-        medicine = selection[0]
-        compound = selection[1] 
-        indication = selection[2]
-        if medicine:
-            st.write('MEDICINE: ', medicine)
-            __search_term = self.__searcher.find_medicines(medicine)
-            __func = self.__searcher.find_medicines
-            __posting = False
-            self.__display_medicines(__search_term, __func, __posting)
-        if compound:
-            st.write('COMPOUND: ', compound)
-            __search_term = self.__searcher.find_principles(compound)
-            __func = self.__searcher.find_principles
-            __posting = False
-            self.__display_medicines(__search_term, __func, __posting)
-        if indication:
-            st.write('INDICATION: ', indication)
-            __search_term = self.__searcher.find_therapeutic_indication(indication)
-            __func = self.__searcher.find_therapeutic_indication
-            __posting = True
-            self.__display_medicines(__search_term, __func, __posting)
-        download_csv = st.button('Download as csv')
-        if download_csv: 
-            self.__handler.to_csv(self.__searcher.nombre,self.__searcher.response_dict)
-        
-    def basic_info(self, response_sing, nregistro):
+    def __display_compounds_info(self, nregistro):
+        self.__searcher.find_medicine(nregistro)
+        response_sing = self.__searcher.search_motor_sing()
+        self.__print_comp_info(response_sing, nregistro)
+             
+    def __print_comp_info(self, response_sing, nregistro):   
         for __key, __value in self.__printer.interest_data_strings.items():
             if response_sing[__value] == True:
                 response_sing[__value] = 'Sí'
@@ -129,18 +129,24 @@ class Motor:
         for __key, __value in self.__printer.interest_data_lists.items():        
             st.write(f'  **{__key}:**')
             for __activep in response_sing[__value]:
-                if st.button(f"{__activep['nombre'].capitalize()}", key=str(nregistro) + __activep['nombre']):
-                    st.write(__activep['nombre'])
-                    self.__retrieve_compound_data(1)
+                if st.session_state.get('med_buttons_deployed', False):
+                    __button_key = str(nregistro) + __activep['nombre']
+                    self.__create_comp_button(button_key=__button_key, label=f"{__activep['nombre'].capitalize()}")
+                    if st.session_state.comp_buttons.get(__button_key, False):
+                        st.write(__activep['nombre'])
+                        self.__print_compound_data(__activep['nombre'])
+                st.session_state.comp_buttons_deployed = False                    
   
-    def __retrieve_compound_data(self, comp_num):
-        __comp_name = self.__handler.get_principle_name(comp_num,self.__searcher.response_sing.json())
-        __en_comp_name = self.__translator.translate_to_en(__comp_name)
-        self.__pubchem.get_compound_props(__en_comp_name.text)
+    def __print_compound_data(self, activep):
+        __en_comp_name = self.__translator.translate_to_en(activep)
+        self.__pubchem.print_compound_props(__en_comp_name.text)
 
-    #Funciones que controlan los inputs
+    #Control Function
 
-    def __check_length(self, search_term, func):
-        while len(search_term) < 3:
-            raise ValueError("Query must be at least 3 characters long")
+    def __check_length(self, search_term):
+        try:
+            if len(search_term) < 3:
+                raise st.error("Query must be at least 3 characters long")
+        except:
+            pass
             
--- drug_finder/streamlit_pubchem.py
+++ drug_finder/streamlit_pubchem.py
@@ -6,18 +6,7 @@ import pubchempy as pcp
 
 class Pubchem:
 
-    '''
-    Esta clase usa métodos de la librería 'pubchempy' para obtener información de los compuestos activos presentes en un medicamento. La librería pubchempy extrae los datos de la base de datos Pubchem dependiente del National Institute of Health de Estados Unidos a través de su API. 
-    La clase ontiene dos funciones:
-
-            - get_compound_props(): se vale del método 'get_compounds' de 'pubchempy' para obtener la informacion de los compuestos presentes en Pubchem que coinciden con el nonmbre del principio activo escogido por el usuario. El método 'get_compounds' devuelve una lista de objetos de tipo 'compound', que tienen un método 'to_dict' para convertir la información de propiedades seleccionadas en un diccionario. La función 'get_compound_props' devuelve la lista de objetos 'compound' y, para cada uno de ellos, imprime por pantalla la información seleccionada. El nombre del principio activo de entrada debe haber sido previamente traducido al inglés por el método 'Translator.translate_to_en'.
-
-            - get_png(): recibe como entradas el nombre de un principio activo y la lista de objetos 'compound' obtenida por 'get_compound_props' a partir de él. Para cada 'compound', descarga mediante el método 'download' de pubchempy un archivo PNG con una representación de su estructura en la carpeta 'outputs'. El nombre de cada archivo PNG es la concatenación del nombre del principio activo y el 'cid' (un identificador único de Pubchem) del compuesto. Para obtener la imagen el método 'download' pide un identificador único del compuesto que en este caso es la 'inchikey'. Por última, imprime un aviso de que las estructuras han sido descargadas.
-
-            - get_compound_dic(): recibe como entrada el nombre de un principio activo en inglés y obtiene el diccionario con todas sus propiedades (en la carpeta temp hay un guardado un ejemplo de este diccionario con el nombre: "ap_props_example.json"). Este diccionario se almacena como variable de clase. Para obtener este diccionario utiliza las funciones de Pubchempy "get_compounds" y "to_dict()". "get_compounds()" devuelve una lista de compuestos o una lista vacía en caso de que la búsqueda no sea fructífera. Dado que solo nos interesa el primer compuesto de esta lista, se obtiene el diccionario del índice [0], para evitar el Keyerror en caso de una búsqueda no fructífera, se incluye una sentencia if. Además, en caso de búsqueda infructuosa, la función devuelve False.
-    '''
-
-    def get_compound_props(self, compound_name):
+    def print_compound_props(self, compound_name):
         __compounds = pcp.get_compounds(compound_name, 'name')
 
         if len(__compounds) == 1:
@@ -28,9 +17,8 @@ class Pubchem:
 
         #cid_list = list()
         for __comp in __compounds:
-            st.write(f"  {__comp}")
+            st.write(f"  **:red[{__comp}]**")
             self.get_png(__comp.cid)
-            #cid_list.append(__comp.cid)
             self.__prop_dict = __comp.to_dict(properties=['cid', 'charge', 'iupac_name', 'molecular_formula', 'molecular_weight'])
             __prop_list = ['CID', 'Charge', 'IUPAC name', 'Molecular formula', 'Molecular weight']
             for __tup in zip(__prop_list,list(self.__prop_dict.values())):
